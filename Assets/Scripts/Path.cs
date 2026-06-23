b2d4b0ad0c97b6792648edbc43a1d9cb010c220c
TymoteuszBroda <tymoteusz.91100@gmail.com>
2026-06-23 18:31:59 +0200

    added labels above waypoints

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEditor;
 
 public class Path : MonoBehaviour
 {
     public GameObject[] waypoints;
-
+    // OnDrawGizmos is used to draw lines betweend waypoints, you can only see them in editor. Not in game view.
     private void OnDrawGizmos()
     {
     if (waypoints.Length > 0)
         {
-            for (int i = 0; i < waypoints.Length - 1; i++)
+            for (int i = 0; i < waypoints.Length; i++)
             {
+                //GUIStyle adds text above waypoints.
+                GUIStyle style = new GUIStyle();
+                style.normal.textColor = Color.white;
+                style.fontSize = 12;
+                style.alignment = TextAnchor.MiddleCenter;
+                Handles.Label(waypoints[i].transform.position + Vector3.up * 0.9f, "Waypoint " + i, style);
+
                 if (i < waypoints.Length - 1)
                 {
                     Gizmos.color = Color.white;
@@ -17,15 +25,5 @@ public class Path : MonoBehaviour
                 }
             }
         }
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
